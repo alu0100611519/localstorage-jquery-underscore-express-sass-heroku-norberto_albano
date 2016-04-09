f9dd6279f2c495e60da31b12b769810540d32a8d
Albano Yanes <ajyanreyu@gmail.com>
2016-04-09 18:46:09 +0100

    añadidos mas test a las pruebas

--- public/test/tests.js
+++ public/test/tests.js
@@ -8,17 +8,29 @@ describe("CSV", function(){
       expect(r[0].value[0]).to.equal('hola');
     });
 
-    it("Deberia reconocer una cadena con coma al final", function(){
+    it("Debería reconocer una cadena con coma al final", function(){
       var cadena = '"hola,"';
       var r = calculate(cadena);
-      expect(r[0].value[0]).to.equal('hola');
+      expect(r[0].value[0]).to.equal('hola,');
     });
 
-    it("Deberia reconocer un float con coma", function(){
+    it("Debería reconocer un float con coma", function(){
       var cadena = '"1,5"';
       var r = calculate(cadena);
-      expect(r[0].value[0]).to.equal('hola');
+      expect(r[0].value[0]).to.equal('1,5');
     });
 
+    it("Debería reconocer una cadena con espacios delante y atras", function(){
+      var cadena = '" hola "';
+      var r = calculate(cadena);
+      expect(r[0].value[0]).to.equal(' hola ');
+    });
+    it("Debería reconocer dos comas seguidas", function() {
+      var cadena = '"hola",,"chuchu"';
+      var r = calculate(cadena);
+      expect(r[0].value[0]).to.equal('hola');
+      expect(r[0].value[1]).to.equal('');
+      expect(r[0].value[2]).to.equal('chuchu');
+    });
   });
 });
